92a617dedcd7606567f8321de9d599f00bc373fd
Puru <puruchoudhary@gmail.com>
2011-03-14 17:48:13 -0700

    Initial version

---
+++ lib/hquote.rb
@@ -0,0 +1,70 @@
+# Hquote: A simple Ruby on Rails plugin to get historical stock quotes from Yahoo
+# Author: Puru Choudhary (puruchoudhary@gmail.com)
+# Repository: git@github.com:medius/hquote.git
+
+# Usage: 
+# quote_req = {:symbol=>"aapl",:start_month=>"0",:start_date=>"1",:start_year=>"2011",
+# :end_month=>"2",:end_date=>"14",:end_year=>"2011",:period=>"d"}
+# my_quote = Hquote.new
+# my_quote.get_ohlcv(quote_req)
+#
+# => [{:date=>"2011-03-11", :open=>"345.33", :high=>"352.32", :low=>"345.00", 
+# :close=>"351.99", :volume=>"16813300", :adj_close=>"351.99"}, {:date=>"2011-03-10", 
+# :open=>"349.12", :high=>"349.77", :low=>"344.90", :close=>"346.67", :volume=>"18126400", 
+# :adj_close=>"346.67"}, .... ]
+
+# Credit: This work is derived from John Yerhot's rQuote plugin
+# git://github.com/johnyerhot/rquote.git
+
+class Hquote
+  require 'cgi'
+  require 'net/http'
+
+  @@service_uri = "http://ichart.finance.yahoo.com/table.csv"
+  #"?s=GE&a=11&b=2&c=2010&d=02&e=14&f=2011&g=d&ignore=.csv"
+  
+  # We pass a quote_req hash that contains all the arguments for our request
+  def get_ohlcv(quote_req)
+    output = Array.new
+    i = 0
+    response = send_request(quote_req)
+    String.new(response).split("\n").each do |line|
+      a = line.chomp.split(",")
+      unless i == 0
+        output << { :date => a[0], 
+                  :open => a[1],
+                  :high => a[2],
+                  :low  => a[3],
+                  :close => a[4],
+                  :volume => a[5],
+                  :adj_close => a[6]
+                }
+      end          
+      i += 1
+    end
+    return output
+  end
+  
+  private
+  def send_request(quote_req)
+    completed_path = @@service_uri + construct_args(quote_req)
+    uri = URI.parse(completed_path)
+    response = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.get completed_path
+    end
+    print "Response:\n" + response.body
+    return response.body
+  end
+  
+  def construct_args(quote_req)
+    path =        "?s=" + quote_req[:symbol]
+    path = path + "&a=" + quote_req[:start_month]
+    path = path + "&b=" + quote_req[:start_date]
+    path = path + "&c=" + quote_req[:start_year]
+    path = path + "&d=" + quote_req[:end_month]
+    path = path + "&e=" + quote_req[:end_date]
+    path = path + "&f=" + quote_req[:end_year]
+    path = path + "&g=" + quote_req[:period]
+  end
+  
+end
